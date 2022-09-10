5dd536b4681821f756d6436c839bcdbf3dd9bfbd
Tayssir <tayssirzrafi@gmail.com>
2022-09-10 18:29:39 +0100

    sale order module

---
+++ sale_order_alert/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Sale Order Alert",
+    "summary": "Sale Order Alert",
+    'version': '1.0',
+    "category": "Inventory/Inventory",
+    "website": "https://slnee.com",
+   	"description": """ This module show a validation alert when 
+       a user added a product in sale order line more than once. """,
+    "author": "Slnee",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ['sale', 'product'],
+    "data": [],
+}
---
+++ sale_order_alert/models/sale_order.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from odoo.exceptions import UserError, Warning
+from odoo import api, fields, models, _
+
+_logger = logging.getLogger(__name__)
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.onchange('order_line')
+    def onchange_product_id(self):
+        self.ensure_one()
+        existing_products = []
+        warning = {}
+        for line in self.order_line:
+            if line.product_id in existing_products:
+                count_product = existing_products.count(line.product_id)
+                title = _("Alerte pour l'article: %s", line.product_id.name)
+                message = _(
+                    "Vous avez ajouté cet article (%s) fois" % (count_product + 1))
+                warning = {
+                    'title': title,
+                    'message': message
+                }
+                return {'warning': warning}
+            existing_products.append(line.product_id)
