06566c9a7746173b88d18aa3c8b245a972bbc9c1
Tayssir <tayssirzrafi@gmail.com>
2022-09-10 19:43:32 +0100

    Update sale_order.py

--- sale_order_alert/models/sale_order.py
+++ sale_order_alert/models/sale_order.py
@@ -15,15 +15,16 @@ class SaleOrder(models.Model):
         self.ensure_one()
         existing_products = []
         warning = {}
-        for line in self.order_line:
-            if line.product_id in existing_products:
+        if self.order_line:
+            existing_products = [line.product_id for line in self.order_line]
+            for line in self.order_line:
                 count_product = existing_products.count(line.product_id)
-                title = _("Alerte pour l'article: %s", line.product_id.name)
-                message = _(
-                    "Vous avez ajouté cet article (%s) fois" % (count_product + 1))
-                warning = {
-                    'title': title,
-                    'message': message
-                }
-                return {'warning': warning}
-            existing_products.append(line.product_id)
+                if line.product_id in existing_products and count_product > 1:
+                    title = _("Alerte pour l'article: %s", line.product_id.name)
+                    message = _("Vous avez ajouté cet article (%s) fois" % count_product)
+                    warning = {
+                        'title': title,
+                        'message': message
+                    }
+                    return {'warning': warning}
+                existing_products.append(line.product_id)
